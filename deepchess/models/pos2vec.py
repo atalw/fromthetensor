12306a2f5da97001f5e3e82af744e68adc1ba2e6
Akshit Talwar <akshittalwar@protonmail.com>
2024-03-23 14:11:22 +0530

    pos2vec train and save

--- deepchess/models/pos2vec.py
+++ deepchess/models/pos2vec.py
@@ -4,39 +4,78 @@ hyp = {
   'epochs':               200,
   'opt': {
     'lr':                 0.005,
-    'lr_decay_decay':     0.98
+    'lr_decay':           0.98
   }
 }
 
 class Pos2Vec:
-  def __init__(self):
-    self.layers = [
-      nn.Linear(773, 600),
-      lambda x: x.relu(),
-      nn.Linear(600, 773),
-      lambda x: x.relu(),
+  def __init__(self, level):
+    if level >= 1:
+      self.level1 = [
+        nn.Linear(773, 600),
+        lambda x: x.relu(),
+        nn.Linear(600, 773),
+      ]
+    if level >= 2:
+      self.fc1 = nn.Linear(773, 600)
+      self.level2 = [
+        nn.Linear(600, 400),
+        lambda x: x.relu(),
+        nn.Linear(400, 600),
+      ]
+    if level >= 3:
+      self.fc2 = nn.Linear(600, 400)
+      self.level3 = [
+        nn.Linear(400, 200),
+        lambda x: x.relu(),
+        nn.Linear(200, 400),
+      ]
+    if level >= 4:
+      self.fc3 = nn.Linear(400, 200)
+      self.level4 = [
+        nn.Linear(200, 100),
+        lambda x: x.relu(),
+        nn.Linear(100, 200),
+      ]
+    if level >= 5:
+      self.fc4 = nn.Linear(200, 100)
 
-      nn.Linear(773, 600),
-
-      nn.Linear(600, 400),
-      lambda x: x.relu(),
-      nn.Linear(400, 600),
-      lambda x: x.relu(),
-
-      nn.Linear(600, 400),
-
-      nn.Linear(400, 200),
-      lambda x: x.relu(),
-      nn.Linear(200, 400),
-      lambda x: x.relu(),
-
-      nn.Linear(400, 200),
-
-      nn.Linear(200, 100),
-      lambda x: x.relu(),
-      nn.Linear(100, 200),
-    ]
-
+  # forward autoencode pass
+  def __call__(self, x: Tensor, level) -> Tensor:
+    if level == 1:
+      return x.sequential(self.level1)
+    elif level == 2:
+      x = self.__call__(x, 1).relu()
+      x = self.fc1(x).relu()
+      return x.sequential(self.level2)
+    elif level == 3:
+      x = self.__call__(x, 2).relu()
+      x = self.fc2(x).relu()
+      return x.sequential(self.level3)
+    elif level == 4:
+      x = self.__call__(x, 3).relu()
+      x = self.fc3(x).relu()
+      return x.sequential(self.level4)
+    elif level == 5:
+      x = self.__call__(x, 4).relu()
+      x = self.fc4(x)
+      return x
   
-  def __call__(self, x: Tensor) -> Tensor:
-    return x.sequential(self.layers)
+  # returns expected output for each level (without autoencode)
+  # used for calculating loss
+  def expected_output(self, x: Tensor, level) -> Tensor:
+    if level == 1:
+      return x
+    elif level == 2:
+      x = self.expected_output(x, 1)
+      return self.fc1(x)
+    elif level == 3:
+      x = self.expected_output(x, 2)
+      return self.fc2(x)
+    elif level == 4:
+      x = self.expected_output(x, 3)
+      return self.fc3(x)
+    elif level == 5:
+      x = self.expected_output(x, 4)
+      return self.fc4(x)
+
--- deepchess/train_pos2vec.py
+++ deepchess/train_pos2vec.py
@@ -1,9 +1,10 @@
 import time
 from typing import Tuple
 import numpy as np
-from tinygrad import Tensor, dtypes, GlobalCounters
+from tinygrad import Tensor, dtypes, GlobalCounters, TinyJit, Device
 from tinygrad.nn import optim
-from tinygrad.nn.state import get_parameters
+from tinygrad.nn.state import get_parameters, get_state_dict, safe_save
+from extra.lr_scheduler import OneCycleLR
 import models.pos2vec as autoencoder 
 from tqdm import trange
 
@@ -14,40 +15,45 @@ def load_data():
   X, Y = dat['arr_0'], dat['arr_1']
   X_train, X_test = X[:int(len(X)*ratio)], X[int(len(X)*ratio):]
   Y_train, Y_test = Y[:int(len(Y)*ratio)], Y[int(len(Y)*ratio):]
-  X_train = Tensor(X_train, requires_grad=False, dtype=dtypes.float32).reshape((-1, 1, 1, 773))
-  X_test = Tensor(X_test, dtype=dtypes.float32).reshape((-1, 1, 1, 773))
+  X_train = Tensor(X_train, dtype=dtypes.float32)
+  X_test = Tensor(X_test, dtype=dtypes.float32)
   Y_train = Tensor(Y_train, dtype=dtypes.float32).unsqueeze(-1)
   Y_test = Tensor(Y_test, dtype=dtypes.float32).unsqueeze(-1)
-  print(X_train.shape, Y_train.shape)
   return X_train, Y_train, X_test, Y_test
 
-def train_step() -> Tuple[Tensor, Tensor]:
-    with Tensor.train():
-        sample = Tensor.randint(BS, high=X_train.shape[0])
-        batch = X_train[sample]
-        labels = Y_train[sample]
+@TinyJit
+def train_step(level) -> Tuple[Tensor, Tensor]:
+  with Tensor.train():
+    sample = Tensor.randint(BS, high=X_train.shape[0])
+    batch = X_train[sample]
+    target = model.expected_output(batch, level)
 
-        out = model(batch)
-        loss = out.sub(labels).square().mean().sqrt()
+    out = model(batch, level)
+    loss = out.sub(target).square().mean().sqrt()
 
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-
-        acc = (out.argmax(axis=-1) == labels).mean()
-        return loss.realize(), acc.realize()
+    opt.zero_grad()
+    loss.backward()
+    opt.step()
+    
+    return loss.realize()
 
 if __name__ == "__main__":
-  X_train, Y_train, X_test, Y_test = load_data()
-  model = autoencoder.Pos2Vec()
-  opt = optim.Adam(get_parameters(model), lr=autoencoder.hyp['opt']['lr'])
   BS = 128
+  autoencoder_level = 5
+  model = autoencoder.Pos2Vec(autoencoder_level)
+  opt = optim.SGD(get_parameters(model), lr=autoencoder.hyp['opt']['lr'])
+  X_train, Y_train, X_test, Y_test = load_data()
 
   st = time.monotonic()
 
   for i in (t := trange(autoencoder.hyp['epochs'])):
     GlobalCounters.reset()
     cl = time.monotonic()
-    loss, acc = train_step()
-    t.set_description(f"loss: {loss.numpy():6.2f} accuracy: {acc.numpy():5.2f} %{GlobalCounters.global_ops*1e-9/(cl-st):9.2f} GFLOPS")
+    loss = train_step(level=autoencoder_level)
+    t.set_description(f"lr: {opt.lr.item():9.7f} loss: {loss.numpy():4.2f} {GlobalCounters.global_ops*1e-9/(cl-st):9.2f} GFLOPS")
+    opt.lr.assign(opt.lr * autoencoder.hyp['opt']['lr_decay'])
     st = cl
+  
+  fn = f"./ckpts/pos2vec.safe"
+  safe_save(get_state_dict(model), fn)
+  print(f" *** Model saved to {fn} ***")
