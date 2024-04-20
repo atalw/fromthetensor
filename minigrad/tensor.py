776d3729f16464edda235fe32111060ab81de9f9
Akshit Talwar <akshittalwar@protonmail.com>
2024-04-21 01:06:01 +0530

    backward pass

--- minigrad/tensor.py
+++ minigrad/tensor.py
@@ -1,20 +1,58 @@
 # used teenygrad as the learning resource - https://github.com/tinygrad/teenygrad
+from typing import Tuple, Optional
+from dtype import Dtype
 
 class Tensor:
-  def __init__():
-    pass
+  def __init__(self, data, device=None, dtype=None, requires_grad=None):
+    if device is None: device = "CPU"
+    assert dtype is None or isinstance(dtype, Dtype), f"invalid dtype {dtype}"
 
+    self.grad: Optional[Tensor] = None
+
+  def __repr__(self):
+    return f"<Tensor {self.data!r} on {self.device} with grad {(self.grad.data if self.grad else None)!r}"
+
+  # *** properties ***
+  @property
+  def device(self) -> str: return self.device
+
+  @property
+  def shape(self) -> Tuple[int]: return self.shape
+
+  @property
+  def dtype(self) -> Dtype: return self.dtype
+
+  # *** backward pass ***
+  # toposort
+  def deepwalk(self):
+    def _deepwalk(node, visited, nodes):
+      visited.add(node)
+      if getattr(node, "_ctx", None):
+        for i in node._ctx.parents:
+          if i not in visited: _deepwalk(i, visited, nodes)
+        nodes.append(node)
+      return nodes
+    return _deepwalk(self, set(), [])
+
+  def backward(self):
+    assert self.shape == tuple(), f"backward can only called for scalar tensors, but it has shape {self.shape}"
+    # first grad is 1
+    self.grad = Tensor(1, device=self.device, requires_grad=False)
+    for t0 in reversed(self.deepwalk()):
+      assert t0.grad is not None
+      grads = t0._ctx.backward(t0.grad.data)
+      gs = []
+      for g in ([grads] if len(t0.ctx.parents) == 1 else grads):
+        gs.append(Tensor(g, device=self.device, requires_grad=False) if g is not None else None)
+      for t, g in zip(t0._ctx.parents, grads):
+        assert g.shape == t.shape, f"grad shape must match tensor shape, {g.shape!r} != {t.shape!r}"
+        t.grad = g if t.grad is None else (t.grad + g)
+      del t0._ctx
+    return self
+    
 
 """
 *** high level tensor ops ***
-
-# backward pass
-deepwalk
-backward
-
-# properties
-shape
-dtype
 
 # data handling
 numpy
@@ -33,42 +71,6 @@ eye
 rand
 randn
 randint
-
-# movement ops
-__getitem__
-__setitem__
-reshape
-expand
-permute
-flip
-shrink
-pad
-slice
-gather
-cat
-stack
-repeat
-chunk
-squeeze
-unsqueeze
-
-# reduce ops
-sum
-max
-min
-mean
-std
-softmax
-log_softmax
-argmax
-avg_pool2d
-max_pool2d
-conv2d
-dot
-matmul
-cumsum
-triu
-tril
 
 # unary function
 neg
@@ -100,9 +102,46 @@ sub
 mul
 div
 pow
+matmul
 
 # ternary
 where
+
+# reduce ops
+sum
+max
+min
+mean
+std
+softmax
+log_softmax
+argmax
+avg_pool2d
+max_pool2d
+conv2d
+dot
+matmul
+cumsum
+triu
+tril
+
+# movement ops
+__getitem__
+__setitem__
+reshape
+expand
+permute
+flip
+shrink
+pad
+slice
+gather
+cat
+stack
+repeat
+chunk
+squeeze
+unsqueeze
 
 # functional
 linear
