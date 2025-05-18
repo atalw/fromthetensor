d80835a6710a57a8d2f6e795182433be64876138
Akshit Talwar <akshittalwar@protonmail.com>
2025-05-18 19:19:19 +0530

    back to basics

---
+++ micrograd/engine.py
@@ -0,0 +1,9 @@
+class Value:
+
+  def __init__(self, data, _children=(), _op=''):
+    self.data = data
+    self.grad = 0
+    # internal variables used for autograd graph construction
+    self._backward = lambda: None
+    self._prev = set(_children)
+    self._op = _op
