3cfe696a1f4033a48e14a900f06c2c065616b188
Akshit Talwar <akshittalwar@protonmail.com>
2025-06-21 16:44:51 +0530

    semi working train loop (buffer allocation issue)

--- rnn/main.py
+++ rnn/main.py
@@ -4,6 +4,7 @@ from string import ascii_letters
 from unidecode import unidecode
 
 from tinygrad import nn, dtypes, Tensor
+from tinygrad.nn.state import get_parameters
 
 data_dir = "./data/names"
 lang2label = {
@@ -48,9 +49,57 @@ def create_dataset():
   test_dataset = list(zip(tensor_names[train_idx:], target_langs[train_idx:]))
   return train_dataset, test_dataset
 
+class RNN():
+  def __init__(self, input_size, hidden_size, output_size):
+    self.hidden_size = hidden_size
+    self.in_to_hidden = nn.Linear(input_size + hidden_size, hidden_size)
+    self.in_to_output = nn.Linear(input_size + hidden_size, output_size)
+
+  def __call__(self, x, hidden):
+    combined = x.cat(hidden, dim=1)
+    hidden = Tensor.sigmoid(self.in_to_hidden(combined))
+    output = self.in_to_output(combined)
+    return output, hidden
+
+  def init_hidden(self):
+    return Tensor.kaiming_uniform(1, self.hidden_size)
+
+def train(train_dataset):
+  with Tensor.train():
+    hidden_size = 64 
+    learning_rate = 0.001
+
+    model = RNN(num_letters, hidden_size, num_langs)
+    opt = nn.optim.Adam(get_parameters(model), learning_rate)
+
+    num_epochs = 2
+    print_interval = 1000
+
+    for epoch in range(num_epochs):
+      random.shuffle(train_dataset)
+      for i, (name, label) in enumerate(train_dataset):
+        hidden = model.init_hidden()
+        for char in name:
+          output, hidden = model(char, hidden)
+        loss = output.sparse_categorical_crossentropy(label)
+        opt.zero_grad()
+        loss.backward()
+        for param in get_parameters(model):
+          param.grad = param.grad.clip(-1, 1)
+        (-1, 1)opt.step()
+
+        if (i + 1) % print_interval == 0:
+          print(
+            f"Epoch [{epoch + 1}/{num_epochs}], "
+            f"Step [{i + 1}/{len(train_dataset)}], "
+            f"Loss: {loss.item():.4f}"
+          )
+
 if __name__ == "__main__":
   # print(name_to_tensor("abc").numpy())
   # print(name_to_tensor_one_hot("abc").numpy())
   train_dataset, test_dataset = create_dataset()
   print("Train: ", len(train_dataset))
   print("Test: ", len(test_dataset))
+
+  train(train_dataset)
